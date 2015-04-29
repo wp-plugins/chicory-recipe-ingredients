e6c305f806613093ab0b97c981babaaed57070d2
Chicory <Chicory@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-29 08:55:52 +0000

    Updated Version
    git-svn-id: https://plugins.svn.wordpress.org/chicory-recipe-ingredients/trunk@1148802 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- chicory-recipe-ingredients.php
+++ chicory-recipe-ingredients.php
@@ -1,222 +1,131 @@
 <?php
 /*
 Plugin Name: Chicory Recipe Ingredients
-Plugin URI: http://www.chicoryapp.com/
+Plugin URI: http://chicoryapp.com/
 Description: Chicory connects your recipes directly to leading online grocery stores. The Chicory recipe plugin displays a “get ingredients delivered’ button before or after your recipe, letting  readers cook the  recipes they discover on your site without even having to make a trip to the grocery store.
-Version: 1.4
+Version: 1.5
 Text Domain: chicory-recipe
 Author: Chicory
-Author URI: http://www.chicoryapp.com/
+Author URI: http://chicoryapp.com/
 */
 
-define( 'CHICORY_PLUGIN_URL', 			plugin_dir_url( __FILE__ ) );
-define( 'CHICORY_PLUGIN_PATH',			plugin_dir_path( __FILE__ ) );
-define( 'CHICORY_PLUGIN_BASENAME', 		plugin_basename( __FILE__ ) );
-define( 'CHICORY_PLUGIN_REL_DIR', 		dirname( PLUGIN_BASENAME ) );
+define('CHICORY_PLUGIN_URL', 			plugin_dir_url(__FILE__));
+define('CHICORY_PLUGIN_PATH',			plugin_dir_path(__FILE__));
+define('CHICORY_PLUGIN_BASENAME', 		plugin_basename(__FILE__));
+define('CHICORY_PLUGIN_REL_DIR', 		dirname(PLUGIN_BASENAME));
+define('CHICORY_WIDGET_DOMAIN',         'localhost:8000');
 
-register_activation_hook( __FILE__, 'chicory_activate' );	
-register_deactivation_hook( __FILE__,'chicory_deactivate' );
-register_uninstall_hook( __FILE__, 'chicory_uninstall'  );
+const BUTTON_LOCATION_BELOW_INGREDIENTS = 'below-ingredient';
+const BUTTON_LOCATION_BELOW_RECIPE      = 'below-recipe';
+const BUTTON_LOCATION_BELOW_POST        = 'below-post';
 
-/**
-*  Activate Plugin : When activate plugin then tabel created and default value added in database.
-*
-* Function Name: chicory_activate
-*
-**/
 function chicory_activate() {			
-	add_option( 'chicory_location_button', 'bottom', '', 'yes' );
+	add_option('chicory_location_button', BUTTON_LOCATION_BELOW_INGREDIENTS, '', 'yes');
 }
 
-/**
-*  Deactivate Plugin : When deactivate plugin then default value deleted in database.
-*
-* Function Name: chicory_deactivate
-*
-**/
-function chicory_deactivate(){
-	delete_option( 'chicory_location_button' );
-}
-	
-/**
-*  Uninstall Plugin : When uninstall plugin default value deleted in database.
-*
-* Function Name: chicory_uninstall
-*
-**/	
-function chicory_uninstall(){	
-	delete_option( 'chicory_location_button' );
+function chicory_deactivate() {
+	delete_option('chicory_location_button');
 }
 
-/**
-* When plugin loaded then all files called.
-*
-* Function Name: chicory_plugin_load_function.
-*
-**/
-function chicory_plugin_load_function(){
-	if ( is_admin() ) {
-		add_action( 'admin_menu', 'chicory_admin_menu' );
-	} else {
-		add_action( 'wp_enqueue_scripts', 'chicory_scripts' );
-		
-		$chicory_location_button = get_option( 'chicory_location_button' );
-		if( $chicory_location_button == 'bottom' ) {
-			add_filter( 'the_content', 'chicory_bottom_recipe_display', 1 );
-		} else if( $chicory_location_button == 'ziplist' ) {
-			add_action( 'wp_head', 'chicory_ziplist_recipe_display' );
-		} else if( $chicory_location_button == 'easyrecipe' ) {
-			add_action( 'wp_head', 'chicory_easyrecipe_recipe_display' );
-		} else if( $chicory_location_button == 'shortcode' ) {
-			add_shortcode( 'chicory', 'chicory_shortcode_recipe_display' );
-		}
-	}	
-}
-add_action( 'plugins_loaded','chicory_plugin_load_function' );
-
-/**
-* When plugin loaded then all script also loaded.
-*
-* Function Name: chicory_scripts.
-*
-**/
-function chicory_scripts() {
-	if(is_single()) {
-		wp_enqueue_script( 'chicory-script', 'http://chicoryapp.com/widget_v2/', array(), '', true );
-	}
+function chicory_uninstall() {
+	delete_option('chicory_location_button');
 }
 
-/**
-* Chicory Recipe Ingredients Settings Menu.
-*
-* Function Name: chicory_admin_menu.
-*
-**/
 function chicory_admin_menu() { 
 	add_menu_page('Chicory Recipe Ingredients', 'Chicory', 'administrator', __FILE__, 'chicory_settings_page', plugins_url('/icon/icon.png', __FILE__), 82);
-
-	add_action( 'admin_init', 'chicory_register_settings' );
+	add_action('admin_init', 'chicory_register_settings');
 }
 
-/**
-* Register Settings.
-*
-* Function Name: chicory_register_settings.
-*
-**/
-function chicory_register_settings() {
-  register_setting( 'chicory-settings-group', 'chicory_location_button' );
-}
-
-/**
-* Chicory Recipe Ingredients Settings Page.
-*
-* Function Name: chicory_settings_page.
-*
-**/
-function chicory_settings_page() { ?>
-	<div class="wrap">
-	<h2><?php _e( 'Chicory Recipe Ingredients', 'chicory-recipe' ) ?></h2>
+function chicory_settings_page() {
+    $option = get_option('chicory_location_button');
+?>
+<div class="wrap">
+	<h2>Chicory Recipe Ingredients</h2>
 	<form method="post" action="options.php">
-		<?php settings_fields( 'chicory-settings-group' ); 
-		do_settings_sections( 'chicory-settings-group' ); 
-		get_option( 'chicory_location_button' ); ?>
+		<?= settings_fields('chicory-settings-group') ?>
+		<?= do_settings_sections('chicory-settings-group') ?>
 		<table class="form-table">
 			<tr valign="top">
-				<th scope="row"><?php _e( 'What location would you like to display the Chicory button?', 'chicory-recipe' ) ?></th>
+				<th scope="row">What location would you like to display the Chicory button?</th>
 			</tr>
 			<tr>
 				<td>
-					<input type="radio" id="chicory_location_button" name="chicory_location_button" value="bottom" <?php echo ((get_option('chicory_location_button') == 'bottom') ? 'checked="checked"' : '') ?> /><?php _e( 'Bottom of recipe', 'chicory-recipe' ) ?><br/><br/>
-					<input type="radio" id="chicory_location_button" name="chicory_location_button" value="ziplist" <?php echo ( ( get_option('chicory_location_button') == 'ziplist' ) ? 'checked="checked"' : '' ) ?> /><?php _e( 'Below Ziplist', 'chicory-recipe' ) ?><br/><br/>
-					<input type="radio" id="chicory_location_button" name="chicory_location_button" value="easyrecipe" <?php echo ( ( get_option('chicory_location_button') == 'easyrecipe' ) ? 'checked="checked"' : '' ) ?> /><?php _e( 'Below EasyRecipe', 'chicory-recipe' ) ?><br/><br/>
-					<input type="radio" id="chicory_location_button" name="chicory_location_button" value="shortcode" <?php echo ( ( get_option('chicory_location_button' ) == 'shortcode' ) ? 'checked="checked"' : '') ?> /><?php _e( 'ShortCode* [chicory]', 'chicory-recipe' ) ?>
-					<p><?php _e( 'Note: * this option lets you have full control over the location of the button. However, it requires to put the short code [chicory] in every post or install it in your template. contact Chicory for the best place to install it in your template.', 'chicory-recipe' ) ?></p>
+					<input type="radio" id="chicory_location_button" name="chicory_location_button" value="<?= BUTTON_LOCATION_BELOW_INGREDIENTS ?>" <?= ($option == BUTTON_LOCATION_BELOW_INGREDIENTS) ? 'checked="checked"' : '' ?> />Below Ingredient List<br/><br/>
+					<input type="radio" id="chicory_location_button" name="chicory_location_button" value="<?= BUTTON_LOCATION_BELOW_RECIPE ?>" <?= ($option == BUTTON_LOCATION_BELOW_RECIPE) ? 'checked="checked"' : '' ?> />Below Recipe<br/><br/>
+					<input type="radio" id="chicory_location_button" name="chicory_location_button" value="<?= BUTTON_LOCATION_BELOW_POST ?>" <?= ($option == BUTTON_LOCATION_BELOW_POST) ? 'checked="checked"' : '' ?> />Bottom of Post<br/><br/>
 				</td>
 			</tr>
 			</tr>
-		</table> 
-		<?php submit_button(); ?>
+		</table>
+		<?= submit_button() ?>
 	</form>
 </div>
-<?php } 
-
-/**
-* Chicory Recipe Ingredients Bottom Content Page Display.
-*
-* Function Name: chicory_bottom_recipe_display.
-*
-**/
-function chicory_bottom_recipe_display( $content ) {
-	$chicory_content  = $content;
-	if(is_single()) {
-		$chicory_content .= '<div class="chicory-order-ingredients"></div>';	
-	} 
-	return $chicory_content;
+<?php
 }
 
-/**
-* Chicory Recipe Ingredients Shortcode Content Page Display.
-*
-* Function Name: chicory_shortcode_recipe_display.
-*
-**/
-function chicory_shortcode_recipe_display() {
-	if(is_single()) {
-		$chicory_content = '<div class="chicory-order-ingredients"></div>';
-		return $chicory_content;
-	}
+function chicory_register_settings() {
+    register_setting('chicory-settings-group', 'chicory_location_button');
 }
 
-/**
-* Chicory Recipe Ingredients Below Ziplist Content Page Display.
-*
-* Function Name: chicory_ziplist_recipe_display.
-*
-**/
-function chicory_ziplist_recipe_display() {
-	if (is_single()) {
-		global $wpdb; 
-		$post_id = get_the_ID();
-		$ziplist = $wpdb->get_row( "SELECT * FROM " . $wpdb->prefix . "posts WHERE post_content LIKE '%amd-zlrecipe-recipe%' AND ID = " . $post_id );
-		if( $ziplist ) {  ?>
-		   <script type="text/javascript" >
-				jQuery(document).ready(function($) {
-					if( jQuery('.zlrecipe-container-border')[0] ) {
-						jQuery('<div class="chicory-order-ingredients"></div>').insertAfter( jQuery('.zlrecipe-container-border') );
-					}
-				});
-			</script>
-		<?php 
-		} else {
-			add_filter( 'the_content', 'chicory_bottom_recipe_display', 1 );
-		}
-	}
+function chicory_scripts() {
+    wp_enqueue_script('chicory-script', 'http://'. CHICORY_WIDGET_DOMAIN . '/widget_v2/', array(), '', true);
 }
 
-/**
-* Chicory Recipe Ingredients Below EasyRecipe Content Page Display.
-*
-* Function Name: chicory_easyrecipe_recipe_display.
-*
-**/
-function chicory_easyrecipe_recipe_display() {
-    if (is_single()) {
-		global $wpdb;
-		$post_id = get_the_ID(); 
-		$easyrecipe = $wpdb->get_row( "SELECT * FROM " . $wpdb->prefix . "posts WHERE post_content LIKE '%easyrecipe%' AND post_content LIKE '%endeasyrecipe%' AND ID = " . $post_id );	
-		if( $easyrecipe ) { ?>
-		   <script type="text/javascript" >
-				jQuery(document).ready(function($) {
-					if( jQuery('.easyrecipe')[0] ) {
-						jQuery('<div class="chicory-order-ingredients"></div>').insertAfter( jQuery('.easyrecipe') );
-					}
-				});			
-			</script>
-		<?php
-		} else {
-			add_filter( 'the_content', 'chicory_bottom_recipe_display', 1 );
-		}
-	}
+function chicory_display($content) {
+    // Check that necessary extensions are present
+    if (!extension_loaded('libxml') || !extension_loaded('dom') || !extension_loaded('mbstring')) {
+        return $content;
+    }
+
+    libxml_use_internal_errors(true);
+    $location = get_option('chicory_location_button');
+    $doc = new DOMDocument();
+    $doc->loadHTML('<?xml encoding="UTF-8">' . mb_convert_encoding($content, 'HTML-ENTITIES', "UTF-8"));
+    $xpath = new DOMXPath($doc);
+
+    // Chicory container
+    $buttonContainer = $doc->createElement('div');
+    $buttonContainer->setAttribute('class', 'chicory-order-ingredients-container');
+    $buttonContainer->setAttribute('style', 'margin-top:10px !important');
+
+    // Microdata format (Easyrecipe, Recipe Card)
+    if ($xpath->query('//*[contains(@itemtype, "//schema.org/Recipe") or contains(@itemtype, "//schema.org/recipe")]')->length) {
+
+        if ($location == BUTTON_LOCATION_BELOW_INGREDIENTS) {
+            $node = $xpath->query('//*[@itemprop="ingredients"]/..')->item(0);
+            $node->parentNode->insertBefore($buttonContainer, $node->nextSibling);
+        }
+
+        if ($location == BUTTON_LOCATION_BELOW_RECIPE) {
+            $node = $xpath->query('//*[contains(@itemtype, "//schema.org/Recipe") or contains(@itemtype, "//schema.org/recipe")]')->item(0);
+            $node->appendChild($buttonContainer);
+        }
+
+        if ($location == BUTTON_LOCATION_BELOW_POST) {
+            $doc->appendChild($buttonContainer);
+        }
+    }
+    else {
+        $doc->appendChild($buttonContainer);
+    }
+
+    $button = $buttonContainer->appendChild($doc->createElement('div'));
+    $button->setAttribute('class', 'chicory-order-ingredients');
+
+    return $doc->saveHTML();
 }
-?>
+
+function chicory_plugin_load_function() {
+    if (is_admin()) {
+        add_action('admin_menu', 'chicory_admin_menu');
+    }
+    else {
+        add_action('wp_enqueue_scripts', 'chicory_scripts');
+        add_filter('the_content', 'chicory_display', 20);
+    }
+}
+
+register_activation_hook(__FILE__, 'chicory_activate');
+register_deactivation_hook(__FILE__,'chicory_deactivate');
+register_uninstall_hook(__FILE__, 'chicory_uninstall' );
+add_action('plugins_loaded','chicory_plugin_load_function');
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: chicory
 Donate link: http://www.chicoryapp.com/
 Tags: chicory, recipe, ingredients, chicoryapp, grocery, store, cook, button
 Requires at least: 3.0
-Tested up to: 3.9.2
-Stable tag: 1.4
+Tested up to: 4.2.1
+Stable tag: 1.5
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -12,37 +12,20 @@ Chicory connects your recipes directly to leading online grocery stores.
 
 == Description ==
 
-Chicory connects your recipes directly to leading online grocery stores.
+Chicory connects your recipes directly to leading online grocery stores like Stop & Shop, Giant and Peapod for home delivery or grocery pickup.
 
-
-
-The Chicory recipe plugin displays a “get ingredients delivered’ button before or after your recipe, letting  readers cook the recipes they discover on your site without even having to make a trip to the grocery store.
-
-Chicory’s intelligent shopping cart makes sure that users get the best products at the best prices for what they’re making whether they’re cooking for one or one hundred.
+The Chicory recipe plugin displays a “Get Ingredients” button right under the list of ingredients on each of your recipes, letting readers cook the recipes they discover on your site without having to make a trip to the grocery store.
 
 Chicory is awesome because:
 
+*   You get access to perks and insights if you become a Chicory recipe partner
+*   It’s super easy to install (just add the plugin, and continue creating recipes as usual).
+*   Integrates seamlessly with the recipe plugin you’re already using
+*   Doesn’t affect your SEO or page load time
 
+[Apply to become a Chicory recipe partner](http://go.chicoryapp.com/become-a-chicory-recipe-partner?utm_referrer=wordpress-plugin-page) to get perks & possible access to a commission per order (depends on size of blog)
 
-*   It’s super easy to install (just add the plugin, select the location of the button, and continue creating recipes like normal).
-
-*   Earn a commission every time a reader order the ingredients to one of your recipes.
-
-*   Have complete control over location of the button, including a short code that allows you to put it anywhere on your template.
-
-*   Doesn’t interfere with Ziplist, Easy Recipe or any other recipe plugins.
-
-Note : The Chicory button only shows up and works if you’re a Chicory recipe partner.
-
- Want to become a Chicory recipe partner?
-	
-	
-
-Please contact us at hello@chicoryapp.com for more information and to chat.
-	
-	
-
-Visit us at [chicoryapp.com](http://chicoryapp.com/) to learn more.
+Check out blog.chicoryapp.com or visit us at [chicoryapp.com](http://chicoryapp.com/) to learn more.
 
 == Installation ==
 
@@ -54,43 +37,50 @@ Follow these steps to install the plugin in your WordPress website:
 
 == Frequently Asked Questions ==
 
-= I already have Ziplist or Easy Recipe. Can I also use Chicory? =
+= What other blogs or websites do you work with? =
+Chicory has recipe partners of all shapes and sizes. We work with Time, Inc. and can be found on MyRecipes.com.
+But bloggers are at our core, with over 60 recipe partners currently on our team.
+We appear on a variety of blogs—from daily recipe sites, to niche publications, to lifestyle blogs.
+Check out some of our partners; Cookistry, Crock-Pot Ladies, Runaway Apricot, Kawaling Pinoy, Real Food by Dad or Hungry Couple.
 
+= How will Chicory use my content, recipes or photos? =
+As bloggers ourselves, we’re committed to sticking up for you and maintaining your content’s integrity.
+We do not believe in repurposing your content without your explicit consent, and we wouldn’t ask to do so unless we felt it was in your interest.
+The agreement you sign with us does grant us access to your photos, which we use for social media posts and on our blog, ALWAYS with a link back to your original post.
 
-Yes! Chicory does not interfere with Ziplist or any other blogger tools or ad networks.
+= Can I make money with Chicory? =
+Yes! Because we are a growing company, our revenue share opportunities begin when our button reaches 100,000 impressions on your site.
+That’s 100,000 impressions, period, not uniques. This policy allows us to on-board blogs for payment on a rolling basis, once we know how a site is performing.
+For smaller blogs, we’ll help you to build audience, and we offer other perks to help you reach that benchmark number.
+We don’t believe in asking bloggers to work for free, and we’re just as invested in getting more traffic to your site as you are!
+Once you do hit this benchmark , we offer 20% commission on profits made on your site. That’s 20% of our profits after we get our cut from the grocer.
+Payments are sent out quarterly. As our customer base grows, and our technology improves, orders may increase on your site.
+Email us if you’re looking for an estimate on how much money you can expect in a quarter once you install Chicory on your site!
+
+= I already have EasyRecipe or another recipe plugin. Can I also use Chicory? =
+Yes! Chicory does not interfere with your recipe plugin  nor any other blogger tools or ad networks.
 
 = Does Chicory affect my SEO? =
+Chicory does not negatively affect your SEO. We do frequently promote your recipes on our social media sites, though, so that should help to positively affect your blog’s search ranking.
 
-
-No. We do not negatively affect your SEO. We do frequently promote your recipes though so it may improve how often your links come up in searches!.
-
-= Do I make money directly from the online grocery sales? =
-
-
-Yes. You earn a commission every time someone orders the ingredients to recipes they found on your site.
-
-= Is there a minimum order? =
-
-
-This depends on the grocer a reader is ordering from and if they choose pickup or delivery. Most pickup locations do not require a minimum order.
-
-
-To meet any minimum requirements, we recommend ordering a few recipes at once.
+= Is there a minimum grocery order when using Chicory? =
+This will depend on the grocer a reader is ordering from and whether the reader chooses pickup or delivery.
+Most pickup locations do not require a minimum order. Minimum delivery sizes will vary depending on the grocer.
+To meet any minimum requirements, we recommend your readers order a few recipes at once.
 
 = Where do you deliver? =
+We currently work with grocers who deliver to 24 major US markets including NYC, Boston, Chicago, DC, Philadelphia, Milwaukee and Indianapolis.
+We are continuously expanding to offer delivery in new areas.
 
-
-We currently work in several hundred zip codes in New York, New Jersey, and Connecticut. We are continuously expanding to offer delivery in new areas.
-
-= What if some of my users don’t have access to online grocer? =
-
-
+= What if some of my users don’t have access to an online grocer or your grocers don’t deliver to their area? =
 Chicory is still a powerful shopping tool that shows readers what national brands they should purchase in store (and the quantity of those brands) when making your recipes.
+Readers can still use Chicory as a powerful shopping tool. Chicory allows readers to build a shopping list that they can then print or email to themselves,
+but will also provide information as to which food brands are available for purchase in local stores.
 
 = What if someone already has ingredients listed in a recipe? =
+Easy! Our tool actually assumes you’ve got certain staples--they will appear greyed out as an optional item for readers.
+Otherwise, Chicory users can deselect ingredients that they already have and those items will be taken out of the shopping cart.
 
-
-Easy! They can deselect the ingredients they already have and they will automatically be taken out of the shopping cart.
 
 == Screenshots ==
 
@@ -99,26 +89,26 @@ Easy! They can deselect the ingredients they already have and they will automati
 
 == Changelog ==
 
-= 1.3 =
+= 1.5 =
+* Updated button location settings.
 
+= 1.4 =
+
+= 1.3 =
 * Resolve bugs of not displaying content and images on blog and index page.
 
 = 1.2 =
-
 * Remove chicory button from front page and other pages.
 * Chicory button only display on single post.
 
 = 1.1 =
-
 * Remove Top Of Recipe Setting.
 * Add Below Ziplist Setting.
 * Add Below EasyRecipe Setting.
 
 = 1.0 =
-
 * First version.
 
 == Upgrade Notice ==
 
-= 1.3 =
 Visit us at [chicoryapp.com](http://chicoryapp.com/) to learn more.
